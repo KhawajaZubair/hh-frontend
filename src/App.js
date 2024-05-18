a55d97053fd9cbeac0d9fbd398fa287242937328
KhawajaZubair <muhammad.zubair.webdev@gmail.com>
2024-05-18 20:18:21 +0500

    removed unwanted imports

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import "./App.css";
 import NavBar from "./components/navbar";
 import Home from "./components/home";
 import Footer from "./components/footer";
-import Appointment from "./components/appointment";
-import Slider from "./components/slider";
-import Doctors from "./components/doctorcard";
-import Blogs from "./components/blogcard";
-import Services from "./components/services";
-import Medicine from "./components/medicine";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
-
       <Footer />
     </div>
   );
