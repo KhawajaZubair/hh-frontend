dba0af0d8fa32c18e9eabdf4900558fd62e31031
KhawajaZubair <muhammad.zubair.webdev@gmail.com>
2024-07-02 19:03:05 +0500

    omar hospital doctors added through api
    omar hospital doctors added through api

--- src/hospitals/omar-hospital/DoctorList.js
+++ src/hospitals/omar-hospital/DoctorList.js
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Nephro01 from "../../Assets/doctors/Nephro01.webp";
-import Nephro02 from "../../Assets/doctors/Nephro02.webp";
-import Derma01 from "../../Assets/doctors/Derma01.webp";
-import Derma02 from "../../Assets/doctors/Derma02.webp";
+import Gastro01 from "../../Assets/doctors/Gastro01.webp";
+import Gastro02 from "../../Assets/doctors/Gastro02.jpeg";
 
-// Image map for specific doctors
 const imageMap = {
-  "Assoc. Prof. Dr. Fowad Shahzad Warraich": Nephro01,
-  "Dr. Hameed Tajammal Khan": Nephro02,
-  "Dr. Sippy Iqbal": Derma01,
-  "Dr. Quratul Ain Sajida": Derma02,
+  "Assist. Prof. Dr. Fahmina Ashfaq": Gastro01,
+  "Dr. Syed Sibtain Ul Hassan": Gastro02,
 };
 
 function DoctorList() {
@@ -29,14 +24,10 @@ function DoctorList() {
         }
         const data = await response.json();
         console.log("Fetched data:", data); // Debug log
-        // Filter doctors by name
-        const filteredDoctors = data.filter((doctor) =>
-          [
-            "Assoc. Prof. Dr. Fowad Shahzad Warraich",
-            "Dr. Hameed Tajammal Khan",
-            "Dr. Sippy Iqbal",
-            "Dr. Quratul Ain Sajida",
-          ].includes(doctor.name)
+        // Filter doctors by hospital
+        const filteredDoctors = data.filter(
+          (doctor) =>
+            doctor.hospital_address === "Omar Hospital & Cardiac Centre"
         );
         console.log("Filtered doctors:", filteredDoctors); // Debug log
         setDoctors(filteredDoctors);
@@ -82,12 +73,12 @@ function DoctorList() {
                     {doctor.specializations?.join(", ")}
                   </p>
                   <p className="card-text">
-                    <strong>Experience:</strong> {doctor.experience}
+                    <strong>Experience:</strong> {doctor.experiences}
                   </p>
                   <div className="d-flex flex-column">
                     <Link
                       to={`/doctors/${doctor.id}`}
-                      className="doc-links"
+                      className="doc-links mb-2"
                     >
                       View Profile
                     </Link>
